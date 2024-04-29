51c4e67ca79c560d852cf8be9c2211b7c59c6cb6
Jhon Lay Lopez <jhonlaylopez000@gmail.com>
2024-04-29 22:41:48 +0800

    create array for object type

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrl: './parent.component.css'
 })
 export class ParentComponent {
-
+  booksToChild: Book[] = [
+    {
+      id: 1,
+      title: "To Kill a Mockingbird",
+      author: "Harper Lee",
+      publishedYear: 1960,
+      genre: "Southern Gothic"
+    },
+    {
+      id: 2,
+      title: "1984",
+      author: "George Orwell",
+      publishedYear: 1949,
+      genre: "Dystopian"
+    },
+    {
+      id: 3,
+      title: "Pride and Prejudice",
+      author: "Jane Austen",
+      publishedYear: 1813,
+      genre: "Romance"
+    }
+  ]
 }
